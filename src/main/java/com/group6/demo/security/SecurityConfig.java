e9159fef2f5da57b85fa2450368995ed6583424b
ChangHee Lee <chbe5082@naver.com>
2021-05-25 13:48:33 +0900

    2021 -05 -25 Login

--- src/main/java/com/group6/demo/security/SecurityConfig.java
+++ src/main/java/com/group6/demo/security/SecurityConfig.java
@@ -1,24 +1,38 @@
 package com.group6.demo.security;
 
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin()
-                .loginPage("/login");
+                .loginPage("/login")
+                .usernameParameter("id")
+                .passwordParameter("pw")
+                .failureUrl("아직미구현")
+                .defaultSuccessUrl("/home");
 
         http.authorizeRequests()
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                 .antMatchers("/*").permitAll()
                 .antMatchers("/login").permitAll()
                 .anyRequest().authenticated();
+
+        http.logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/home")
+                .permitAll();
     }
+
+
+
 
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -27,6 +41,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("{noop}1234").roles("MEMBER","ADMIN");
+
     }
+
+
+
+
+
 }
