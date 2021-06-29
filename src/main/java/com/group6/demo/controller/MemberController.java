09d8c7a018c59e36ce492ad357100937e8c20c2e
YJBAEK <wodnrdlgla@naver.com>
2021-06-30 00:12:21 +0900

    20210630 modify detail , login css

--- src/main/java/com/group6/demo/controller/MemberController.java
+++ src/main/java/com/group6/demo/controller/MemberController.java
@@ -1,7 +1,6 @@
 package com.group6.demo.controller;
 
 import com.group6.demo.entity.login.Member;
-import com.group6.demo.entity.login.MemberDTO;
 import com.group6.demo.repository.MemberRepository;
 import com.group6.demo.security.SignUpFormValidator;
 import com.group6.demo.service.MemberService;
@@ -12,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
 
@@ -36,7 +36,7 @@ public class MemberController {
 			Member member = memberRepository.findMemberByAccount(principal.getName());
 			log.info("memeber : " + member);
 			model.addAttribute("member", member);
-			return "/home/login/mypage";
+			return "/home/login/MyPage";
 		} catch (NullPointerException e){
 			return "redirect:/login";
 		}
@@ -44,24 +44,50 @@ public class MemberController {
 
 	// 마이페이지 회원정보 변경 GET
 	@GetMapping("memberUpdate")
-	public String memberUpdate(Principal principal) {
+	public String memberUpdate(Principal principal, Model model) {
 		try {
-			return "/home/login/mypage";
+			Member member = memberRepository.findMemberByAccount(principal.getName());
+			model.addAttribute("member", member);
+			return "/home/login/modify";
 		} catch (NullPointerException e){
 			return "redirect:/login";
 		}
 
 	}
 
-	// 마이페이지 회원정보 변경 POST
-	@PostMapping("/memberUpdate")
-	public String memberInfoUpdate(MemberDTO dto, Principal principal) {
+	// 마이페이지 패스워드 변경
+	@PostMapping("/memberUpdate/password")
+	public String memberInfoUpdate(@RequestParam String newPassword, Principal principal) {
 		try{
-			memberService.changeAllByAccount(dto);
-			return "redirect:/home";
+			memberService.changePasswordByAccount(principal.getName(),newPassword);
+
+			return "redirect:/mypage";
 		} catch (NullPointerException e){
 			return "redirect:/login";
 		}
 	}
 
+	// 마이페이지 이메일 변경
+	@PostMapping("/memberUpdate/email")
+	public String memberUpdateEmail(@RequestParam String newEmail, Principal principal){
+		try{
+			memberService.changeEmailByAccount(principal.getName(), newEmail);
+
+			return "redirect:/mypage";
+		} catch (NullPointerException e){
+			return "redirect:/login";
+		}
+	}
+
+	// 마이페이지 이름 변경
+	@PostMapping("/memberUpdate/name")
+	public String memberUpdateName(@RequestParam String newName, Principal principal){
+		try{
+			memberService.changeNameByAccount(principal.getName(), newName);
+
+			return "redirect:/mypage";
+		} catch (NullPointerException e){
+			return "redirect:/login";
+		}
+	}
 }
